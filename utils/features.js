ff880fd213c23e37b26a7a06208a415636656361
Kiran18072001 <kumarakiran393@gmail.com>
2024-01-16 20:54:51 +0530

    changes in user.js and features.js

--- utils/features.js
+++ utils/features.js
@@ -6,8 +6,8 @@ export const sendCookie = (user, res, message, statusCode) => {
     res.status(statusCode).cookie("token", token, {
         httpOnly: true,
         maxAge: 40 * 60 * 1000,
-        sameSite: process.env.NODE_ENV === "Development"? "lax":"none",
-        secure: process.env.NODE_ENV === "Development" ? false : true
+        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "Development" ? false : true,
     }).json({
         success: true,
         message
